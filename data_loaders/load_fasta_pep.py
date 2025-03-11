140b9c06fe8246ecf4db9f3ef48cd04491f22f95
nsheff <nsheff@users.noreply.github.com>
2025-03-11 11:08:28 -0400

    Improve tracking info while loading fasta files

--- data_loaders/load_fasta_pep.py
+++ data_loaders/load_fasta_pep.py
@@ -1,16 +1,24 @@
 import pephubclient
 from refget.agents import RefgetDBAgent
 import os
+import time  # Add this import
 
 phc = pephubclient.PEPHubClient()
 p = phc.load_project("nsheff/pangenome_fasta")
 fa_root = os.path.expandvars("$BRICKYARD/datasets_downloaded/pangenome_fasta/2023_hprc_draft")
 
 rga = RefgetDBAgent()
-for s in p.samples:
-    print(f"Loading {s.fasta}")
+total_files = len(p.samples)
+for i, s in enumerate(p.samples, 1):
     fa_path = os.path.join(fa_root, s.fasta)
+    print(f"Loading {fa_path} ({i} of {total_files})")
+    
+    start_time = time.time()  # Record start time
     rga.seqcol.add_from_fasta_file(fa_path)
+    elapsed_time = time.time() - start_time  # Calculate elapsed time
+    
+    print(f"Loaded in {elapsed_time:.2f} seconds")
+    
 
 ## Say I want to add the digest now back into the PEP on PEPhub.
 # What's the best way to do it?
