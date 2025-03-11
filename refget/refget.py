52723b7d4eb7275b5f3f212cf92075c4f47a8feb
nsheff <nsheff@users.noreply.github.com>
2025-03-11 12:01:55 -0400

    add digest-fasta from CLI capability

--- refget/refget.py
+++ refget/refget.py
@@ -7,8 +7,6 @@ import requests
 from copy import copy
 
 from .agents import RefgetDBAgent
-
-
 _LOGGER = logging.getLogger(__name__)
 
 henge.ITEM_TYPE = "_item_type"
@@ -36,21 +34,47 @@ def build_argparser():
         version=version_str,
     )
     subparsers = parser.add_subparsers(dest="command")
-    add_fasta = subparsers.add_parser("add_fasta", help="Add a fasta file to the database")
+
+    add_fasta = subparsers.add_parser("add-fasta", help="Add a fasta file to the database")
     add_fasta.add_argument("fasta_file", help="Path to the fasta file")
+
+    digest_fasta = subparsers.add_parser("digest-fasta", help="Digest a fasta file")
+    digest_fasta.add_argument("fasta_file", help="Path to the fasta file")
+    digest_fasta.add_argument("--level", "-l", help="Output level, one of 0, 1 or 2 (default).", default=2, type=int)
+
     return parser
 
+
+class BytesEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, bytes):
+            return obj.decode(errors="ignore")  # or use base64 encoding
+        return super().default(obj)
 
 def main():
     parser = build_argparser()
     args = parser.parse_args()
-    print(args)
+    _LOGGER.info(args)
 
     if args.command == "add-fasta":
-        print("Adding fasta file")
-        print(args.fasta_file)
+        _LOGGER.info(f"Adding fasta file: {args.fasta_file}")
         refget = RefgetDBAgent()
         refget.seqcol.add_from_fasta_file(args.fasta_file)
-        print("Added fasta file")
+        _LOGGER.info("Added fasta file")
+    elif args.command == "digest-fasta":
+        _LOGGER.info(f"Digesting fasta file: {args.fasta_file}")
+        if args.level == 0:
+            from .utilities import fasta_to_digest
+            digest = fasta_to_digest(args.fasta_file)
+            print(digest)
+        elif args.level == 1:
+            from .utilities import fasta_to_seqcol_dict, seqcol_dict_to_level1_dict
+            seqcol_dict = fasta_to_seqcol_dict(args.fasta_file)
+            level1_dict = seqcol_dict_to_level1_dict(seqcol_dict)
+            print(json.dumps(level1_dict, indent=2))
+        elif args.level == 2:
+            from .utilities import fasta_to_seqcol_dict
+            seqcol_dict = fasta_to_seqcol_dict(args.fasta_file)
+            print(json.dumps(seqcol_dict, indent=2, cls=BytesEncoder))
     else:
         print("No command given")
