5545c181ce136aaccb2a51119c77c122f773e2f4
nsheff <nsheff@users.noreply.github.com>
2025-03-11 21:11:14 -0400

    update service-info endpoint

--- seqcolapi/const.py
+++ seqcolapi/const.py
@@ -9,7 +9,7 @@ ALL_VERSIONS = {
     "seqcolapi_version": seqcolapi_version,
     "refget_pkg_version": refget_pkg_version,
     "python_version": python_version(),
-    "seqcol_spec_version": "0.1.0",
+    "seqcol_spec_version": "1.0.0",
 }
 STATIC_DIRNAME = "static"
 STATIC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), STATIC_DIRNAME)
