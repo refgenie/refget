9294d37c344af2700060dd8a8d102acb5ca070f5
nsheff <nsheff@users.noreply.github.com>
2025-03-20 16:49:30 -0400

    Improve local comparison interpretation module

--- frontend/src/pages/ComparisonInput.jsx
+++ frontend/src/pages/ComparisonInput.jsx
@@ -3,14 +3,35 @@
 // data.
 
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ComparisonView } from './ComparisonView.jsx';
 import { API_BASE } from '../utilities.jsx';
+import { useSearchParams } from 'react-router-dom';
+
 
 
 const ComparisonInput = () => {
   const [comparison, setComparison] = useState(null);
   const [comparisonStr, setComparisonStr] = useState('');
+  const [searchParams] = useSearchParams();
+
+
+  // get state from query param if it exists
+  useEffect(() => {
+    const comparisonFromQuery = searchParams.get('val');
+    if (comparisonFromQuery) {
+      console.log('Encoded comparison from query param:', comparisonFromQuery);
+      // decode base64encoded string
+      const decodedComparisonFromQuery = atob(comparisonFromQuery);
+      // prettify the comparison string
+      const prettyComparison = JSON.stringify(JSON.parse(decodedComparisonFromQuery), null, 2);
+      console.log('Decoded comparison from query:', prettyComparison)
+      setComparisonStr(prettyComparison);
+
+      const parsedComparison = JSON.parse(decodedComparisonFromQuery);
+      setComparison(parsedComparison);
+    }
+  }, [searchParams]);
 
   const handleComparisonChange = (event) => {
     setComparisonStr(event.target.value);
@@ -19,12 +40,22 @@ const ComparisonInput = () => {
   const handleComparisonSubmit = () => {
     const comparison = JSON.parse(comparisonStr);
     setComparison(comparison);
+    // update the query param to base64 encoded string
+    const base64encodedComparison = btoa(comparisonStr);
+    window.history.pushState({}, '', `${window.location.pathname}?val=${base64encodedComparison}`);
+  };
+
+  const clearComparison = () => {
+    console.log("Clearing comparison");
+    setComparisonStr('');
+    setComparison(null);
+    window.history.pushState({}, '', `${window.location.pathname}`);
   };
 
   return (
     <div>
-      <h2>Comparison Input</h2>
-        <p>Paste the output of `/comparison` here:</p>
+      <h2>Comparison Interpretation Module</h2>
+        <p>This tool runs a local interpretation of the output of a sequence collection comparison. Paste the output of `/comparison` here:</p>
       <textarea
         value={comparisonStr}
         onChange={handleComparisonChange}
@@ -34,9 +65,13 @@ const ComparisonInput = () => {
       <button
         onClick={handleComparisonSubmit}
         className="btn btn-primary mt-3"
-      >
-        Submit
-      </button>
+      > Submit </button>
+
+      <button onClick={clearComparison} 
+        className="btn btn-primary mt-3 mx-3">Clear</button>
+
+      <hr/>
+
       {comparison && <ComparisonView paramComparison={comparison} />}
     </div>
   );
--- frontend/src/pages/ComparisonView.jsx
+++ frontend/src/pages/ComparisonView.jsx
@@ -3,38 +3,68 @@ import { useLoaderData, Link } from "react-router-dom"
 import { API_BASE } from "../utilities.jsx"
 
 
-
-const CoordSystemReport = ({message}) => {
+const CoordSystemReport = ({ messageArray }) => {
   return (
-    <div>
-      <div className="alert alert-warning">
-      <h3>Coordinate System</h3>
-        <p>{message}</p>
-      </div>
+    <div className="alert alert-warning">
+      <h3 className="alert-heading">
+        Coordinate System<span 
+          className="ms-2 badge bg-secondary text-white" 
+          data-bs-toggle="tooltip" 
+          data-bs-placement="right"
+          title="This assessment reports on the compatibility of the names and lengths of the sequences, without regard to sequence content."
+          style={{ cursor: 'pointer', fontSize: '0.7rem' }}
+        >
+          ?
+        </span>
+      </h3>
+      <hr />
+      <ul>{messageArray.map((msg, index) => (<li key={index}>{msg}</li>))}</ul>
     </div>
-  )
-}
+  );
+};
+
+const SequencesReport = ({messageArray}) => {
+  return (
+    <div className="alert alert-warning">
+      <h3 className="alert-heading">
+        Sequences<span 
+          className="ms-2 badge bg-secondary text-white" 
+          data-bs-toggle="tooltip" 
+          data-bs-placement="right"
+          title="This assessment reports on the sequences only, without regard to their names."
+          style={{ cursor: 'pointer', fontSize: '0.7rem' }}
+        >
+          ?
+        </span>
+      </h3>
+      <hr />
+      <ul>{messageArray.map((msg, index) => (<li key={index}>{msg}</li>))}</ul>
+    </div>
+  );
+};
+
+
 
 // Component to display the comparison between two collections
 // ✅❔❌❔
-const CoordinateSystemInterpretation = ({comparison}) => {
+const coordinateSystemInterpretation = (comparison) => {
   const lengthsANotB = comparison.array_elements.a.lengths - comparison.array_elements.a_and_b.lengths
   const lengthsBNotA = comparison.array_elements.b.lengths - comparison.array_elements.a_and_b.lengths
   const namesANotB = comparison.array_elements.a.names - comparison.array_elements.a_and_b.names
   const namesBNotA = comparison.array_elements.b.names - comparison.array_elements.a_and_b.names
   const nlpANotB = comparison.array_elements.a.name_length_pairs - comparison.array_elements.a_and_b.name_length_pairs
   const nlpBNotA = comparison.array_elements.b.name_length_pairs - comparison.array_elements.a_and_b.name_length_pairs
+  const msgArray = []
   // If the name_length_pairs match, then the coordinate systems are identical
   if (nlpANotB === 0 && nlpBNotA === 0) {
-    return <CoordSystemReport message="🟰The coordinate systems are identical"/>
+    msgArray.push("🟰 The coordinate systems are identical")
   } else if (nlpANotB === 0 && nlpBNotA > 0) {  // If A nlp is a subset of B
-    return <CoordSystemReport message="Collection A's coordinate system is a subset of B's."/>
+    msgArray.push("Collection A's coordinate system is a subset of B's.")
   } else if (nlpANotB > 0 && nlpBNotA === 0) {  // If B nlp is a subset of A
-    return <CoordSystemReport message="Collection B's coordinate system is a subset of A's."/>
+    msgArray.push("Collection B's coordinate system is a subset of A's.")
   } else if (comparison.array_elements.a_and_b.name_length_pairs !== 0) {  // If there is some overlap
-    return <CoordSystemReport message="The coordinate systems are partially overlapping."/>
+    msgArray.push("The coordinate systems are partially overlapping.")
   } else {  // If there is no overlap
-    const msgArray = []
     msgArray.push("The coordinate systems are disjoint.")
     // If the lengths match and names match
     if (lengthsANotB === 0 && lengthsBNotA === 0 && namesANotB === 0 && namesBNotA === 0) {
@@ -44,11 +74,23 @@ const CoordinateSystemInterpretation = ({comparison}) => {
     } else if (namesANotB === 0 && namesBNotA === 0) {  // If names match but lengths don't
       msgArray.push("✅ Names match. ⚠️ Lengths don't match.")
     }
-    return <CoordSystemReport message={msgArray.join(" ")}/>
   }
-  return <CoordSystemReport message="I'm not sure what's going on with the coordinate systems."/>
+  // msgArray.push("I'm not sure what's going on with the coordinate systems.")
+  return msgArray
 }
 
+const LinkToLocalComparison = ({ comparison }) => {
+  const base64encodedComparison = btoa(JSON.stringify(comparison));
+  return (
+    <a 
+      href={`/comparison?val=${base64encodedComparison}`} 
+      title="View this comparison in Interpretation Module"
+      className="text-decoration-none"
+      >
+      <i className="bi bi-search"> View Interpretation Module</i>
+    </a>
+  );
+}
 
 
 const ComparisonView =({ paramComparison }) => { 
@@ -61,34 +103,43 @@ const ComparisonView =({ paramComparison }) => {
 
     // Do some analysis for interpretation
 
-
+// ✅❔❌
   const getInterpretation = (comparison, attribute) => {
     const nSequencesA = comparison.array_elements.a[attribute]
     const nSequencesB = comparison.array_elements.b[attribute]
     const aNotB = comparison.array_elements.a[attribute] - comparison.array_elements.a_and_b[attribute]
     const bNotA = comparison.array_elements.b[attribute] - comparison.array_elements.a_and_b[attribute]
-    let interpretation = ""
+    const orderCheck = comparison.array_elements.a_and_b_same_order[attribute]
+
     let interpTerm = ""
+    const msgArray = []
 
     if (comparison.array_elements.a_and_b[attribute] == nSequencesA && comparison.array_elements.a_and_b[attribute] == nSequencesB) {
-      interpretation = `The ${attribute} contents are identical.`
+      msgArray.push(`🟰 The ${attribute} contents are identical.`)
+      if (orderCheck === true) {
+        msgArray.push("✅ The elements are in the same order.")
+      } else if (orderCheck === false) {
+        msgArray.push("❌ The elements are in different order.")
+      }
       interpTerm = "identical_content"
-    } else if (comparison.array_elements.a_and_b[attribute] == nSequencesA && comparison.array_elements.a_and_b[attribute] < nSequencesB) {
-      interpretation = `Collection B contains all ${nSequencesA} ${attribute} from collection A, and ${bNotA} additional.`
+    } 
+    if (comparison.array_elements.a_and_b[attribute] == nSequencesA && comparison.array_elements.a_and_b[attribute] < nSequencesB) {
+      msgArray.push(`Collection B contains all ${nSequencesA} ${attribute} from collection A, and ${bNotA} additional.`)
       interpTerm = "subset"
-    } else if (comparison.array_elements.a_and_b[attribute] == nSequencesB && comparison.array_elements.a_and_b[attribute] < nSequencesA) {
-      interpretation = `Collection A contains all ${nSequencesB} ${attribute} from collection B, and ${aNotB} additional.`
+    }
+     if (comparison.array_elements.a_and_b[attribute] == nSequencesB && comparison.array_elements.a_and_b[attribute] < nSequencesA) {
+      msgArray.push(`Collection A contains all ${nSequencesB} ${attribute} from collection B, and ${aNotB} additional.`)
       interpTerm = "subset"
-    } else if (comparison.array_elements.a_and_b[attribute] === 0){
-      interpretation = `The collections' ${attribute} contents are disjoint.`
+    } 
+    if (comparison.array_elements.a_and_b[attribute] === 0){
+      msgArray.push(`The collections' ${attribute} contents are disjoint.`)
       interpTerm = "disjoint"
     } else if (comparison.array_elements.a_and_b[attribute] < nSequencesA && comparison.array_elements.a_and_b[attribute] < nSequencesB) {
-      interpretation = `The collections' ${attribute} contents are partially overlapping; some are shared, and some are unique to each collection.`
+      msgArray.push(`The collections' ${attribute} contents are partially overlapping; some are shared, and some are unique to each collection.`)
       interpTerm = "partial_overlap"
-    } else {
-      interpretation = "This is hard for me to interpret."
     }
-    return {interpretation, interpTerm}
+    
+    return msgArray
   }
 
   const attributesToCheck = ["sequences", "names", "lengths", "name_length_pairs"]
@@ -96,10 +147,14 @@ const ComparisonView =({ paramComparison }) => {
   for (let attribute of attributesToCheck) {
     interpretation[attribute] = getInterpretation(comparison, attribute)
   }
-  console.log(interpretation)
+  // console.log(interpretation)
+  console.log("Comparison:", comparison)
+  const coordSystemMessages = coordinateSystemInterpretation(comparison)
+
     return (
       <div>
         <h2>Comparison</h2>
+        <LinkToLocalComparison comparison={comparison} />
         <h3>Collections being compared</h3>
         <div className="d-flex">
           <label className="col-sm-3 d-flex fw-bold justify-content-end px-4">Digest A:</label>
@@ -111,19 +166,16 @@ const ComparisonView =({ paramComparison }) => {
         </div>
 
         <h2>Interpretation Summary</h2>        
-          <div className="alert alert-success" name="Interpretation">
-            <ul>
-        {comparison.attributes.a_only.length === 0 && comparison.attributes.b_only.length === 0 && (
-              <li>Attributes are identical. </li>
-            )}
-            <li>{interpretation["sequences"].interpretation}</li>
-            <li>{interpretation["names"].interpretation}</li>
-            <li>{interpretation["lengths"].interpretation}</li>
-            <li>{interpretation["name_length_pairs"].interpretation}</li>
-          </ul>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-6">
+                <SequencesReport messageArray={interpretation["sequences"]} />
+                </div>
+                <div className="col-md-6">
+                <CoordSystemReport messageArray={coordSystemMessages}/>
+                </div>
+              </div>
             </div>
-            <CoordinateSystemInterpretation comparison={comparison} />
-
 
         <h2>Details</h2>
         <h3>Attributes</h3>
