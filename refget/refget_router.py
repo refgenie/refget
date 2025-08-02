c35a9b7be76ba404e9870b76c72abbd7a6a58d64
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-08-02 06:35:39 -0600

    use get_many to speed up

--- refget/refget_router.py
+++ refget/refget_router.py
@@ -199,12 +199,14 @@ async def calc_similarities(
     _LOGGER.info("Calculating Jaccard similarities...")
     try:
         collections = dbagent.seqcol.list_by_offset(limit=page_size, offset=page * page_size)
+        seqcols = dbagent.seqcol.get_many(digests=collections["results"])
+
         seqcolA = dbagent.seqcol.get_one_seqcol(digest=collection_digest)
 
         similarities = []
-        for seqcolB in collections["results"]:
-            jaccard_sims = dbagent.calc_similarities_seqcol_dicts(seqcolA, seqcolB.level1())
-            similarities.append({"digest": seqcolB.digest, "similarities": jaccard_sims})
+        for key in seqcols.keys():
+            jaccard_sims = dbagent.calc_similarities_seqcol_dicts(seqcolA, seqcols[key])
+            similarities.append({"digest": key, "similarities": jaccard_sims})
 
         result = {
             "reference_digest": collection_digest,
@@ -216,7 +218,7 @@ async def calc_similarities(
         _LOGGER.debug(e)
         raise HTTPException(
             status_code=404,
-            detail="Error: collection not found. Check the digest and try again.",
+            detail=f"Error: collection not found. Check the digest and try again.{e}",
         )
 
     return JSONResponse(result)
@@ -242,11 +244,11 @@ async def calc_similarities_from_json(
 
     try:
         collections = dbagent.seqcol.list_by_offset(limit=page_size, offset=page * page_size)
+        seqcols = dbagent.seqcol.get_many(digests=collections["results"])
         similarities = []
-
-        for seqcolB in collections["results"]:
-            jaccard_sims = dbagent.calc_similarities_seqcol_dicts(seqcolA, seqcolB.level1())
-            similarities.append({"digest": seqcolB.digest, "similarities": jaccard_sims})
+        for key in seqcols.keys():
+            jaccard_sims = dbagent.calc_similarities_seqcol_dicts(seqcolA, seqcols[key])
+            similarities.append({"digest": key, "similarities": jaccard_sims})
 
         result = {"pagination": collections["pagination"], "similarities": similarities}
 
