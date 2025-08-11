f1b917074681940894dbc2c5d4344068b513096a
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-08-11 16:12:44 -0600

    refactor creating model using rust bindings

--- refget/models.py
+++ refget/models.py
@@ -354,7 +354,7 @@ class SequenceCollection(SQLModel, table=True):
 
         seqcol = SequenceCollection(
             digest=gtars_seq_col.digest,
-            human_readable_name=None,
+            human_readable_names=None,
             sequences=sequences_attr,
             sorted_sequences=sorted_sequences_attr,
             names=names_attr,
